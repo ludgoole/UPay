7e84b36675f5074b6f238da024499035b6a1bf6a
hengxiao <hengxiao@iflytek.com>
2023-08-02 22:48:26 +0800

    Updates

--- login.70af5e12.js
+++
@@ -1,1 +0,0 @@
-import{P as r}from"./assets/index.7546eead.js";const i=t=>r("/client/register",t),o=t=>r("/client/login",t);export{o as l,i as r};
---
+++ login.bb1edf72.js
@@ -0,0 +1,1 @@
+import{P as r}from"./assets/index.2bc434ed.js";const i=t=>r("/client/register",t),o=t=>r("/client/login",t);export{o as l,i as r};
